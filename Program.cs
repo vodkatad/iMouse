6a0ec95140cd84f490df4703ffdd77f3fc36f80a
Elena Grassi <grassi.e@gmail.com>
2014-02-14 18:37:24 +0100

    Added command line support

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 using System;
+using Mono.Options;
+using System.Collections.Generic;
 
 namespace iMouse
 {
@@ -6,7 +8,41 @@ namespace iMouse
 	{
 		public static void Main (string[] args)
 		{
-			Console.WriteLine ("Hello World!");
+			string fileName = "";
+			bool showHelp = false;
+
+			Console.WriteLine("Hello World!");
+			var p = new OptionSet () {
+				{ "f|file=", "the ods file",
+					(string v) => file = v },
+				{ "h|help", "show this message and exit",
+					v => showHelp = v != null },
+			};
+			List<string> extraArgs = null;
+			try {
+				extraArgs = p.Parse(args);
+			} catch (OptionException oe) {
+				Console.Error.WriteLine(oe.Message);
+				return -1;
+			}
+			if (extraArgs.Count != 0 || fileName == "") {
+				ShowHelp(p);
+				return -1;
+			}
+			if (showHelp) {
+				ShowHelp(p);
+				return 0;
+			}
+
+			//https://github.com/paulyoder/LinqToExcel	
+			//http://www.gemboxsoftware.com/SampleExplorer/Spreadsheet/CommonUses/Reading?tab=cs
+		}
+
+		static void ShowHelp(OptionSet p)
+		{
+			Console.WriteLine("Usage: {0} -f filename", Environment.GetCommandLineArgs()[0]);
+			Console.WriteLine("Options:");
+			p.WriteOptionDescriptions(Console.Out);
 		}
 	}
 }
